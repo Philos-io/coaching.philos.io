533d7eca2c7c4daa02d0852bda27fcf5dbe5d5f2
davy engone <davy@philos.io>
2015-11-03 12:22:57 +0100

    initial commit: application to manage coaching sessions by Philos

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import angular, {bootstrap, module} from 'angular';
+
+// Entry point of the application
+module('coaching.philos.io', []);
+
+// Bootstrap the application
+bootstrap(document.body, ['coaching.philos.io']);
+
